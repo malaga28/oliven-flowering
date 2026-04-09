a5e6f1fc92c918845bac66443edc671b8f403746
malaga28 <sebastianpiasecki2710@gmail.com>
2026-04-09 16:30:07 +0200

    Update layers.js

--- config/layers.js
+++ config/layers.js
@@ -1,18 +1,22 @@
 const layersConfig = {
-  historical: {
-    "1951-1980": "Olive_Flowering_MPI_HISTORICAL_1951_1980.tif",
-    "1981-2014": "Olive_Flowering_MPI_HISTORICAL_1981_2014.tif"
-  },
-  future: {
-    ssp245: {
-      "2015-2040": "Olive_Flowering_MPI_SSP245_2015_2040.tif",
-      "2041-2070": "Olive_Flowering_MPI_SSP245_2041_2070.tif",
-      "2071-2100": "Olive_Flowering_MPI_SSP245_2071_2100.tif"
+  floweringRasters: {
+    historical: {
+      "1951-1980": "Olive_Flowering_MPI_HISTORICAL_1951_1980.tif",
+      "1981-2014": "Olive_Flowering_MPI_HISTORICAL_1981_2014.tif"
     },
-    ssp585: {
-      "2015-2040": "Olive_Flowering_MPI_SSP585_2015_2040.tif",
-      "2041-2070": "Olive_Flowering_MPI_SSP585_2041_2070.tif",
-      "2071-2100": "Olive_Flowering_MPI_SSP585_2071_2100.tif"
+    future: {
+      ssp245: {
+        "2015-2040": "Olive_Flowering_MPI_SSP245_2015_2040.tif",
+        "2041-2070": "Olive_Flowering_MPI_SSP245_2041_2070.tif",
+        "2071-2100": "Olive_Flowering_MPI_SSP245_2071_2100.tif"
+      },
+      ssp585: {
+        "2015-2040": "Olive_Flowering_MPI_SSP585_2015_2040.tif",
+        "2041-2070": "Olive_Flowering_MPI_SSP585_2041_2070.tif",
+        "2071-2100": "Olive_Flowering_MPI_SSP585_2071_2100.tif"
+      }
     }
-  }
+  },
+
+  oliveRaster: "olives_4326.tif"
 };
