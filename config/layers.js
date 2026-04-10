ec8acf96365aacf4d753214a8bd0358b8ad09793
malaga28 <sebastianpiasecki2710@gmail.com>
2026-04-10 09:55:21 +0200

    Update layers.js

--- config/layers.js
+++ config/layers.js
@@ -18,5 +18,24 @@ const layersConfig = {
     }
   },
 
-  oliveRaster: "olives_4326.tif"
+  oliveRaster: "olives_4326.tif",
+
+  chartCSVs: {
+    historical: {
+      "1951-1980": "1951.csv",
+      "1981-2014": "1981.csv"
+    },
+    future: {
+      ssp245: {
+        "2015-2040": "2015_ssp245.csv",
+        "2041-2070": "2041_ssp245.csv",
+        "2071-2100": "2071_ssp245.csv"
+      },
+      ssp585: {
+        "2015-2040": "2015_ssp585.csv",
+        "2041-2070": "2041_ssp585.csv",
+        "2071-2100": "2071_ssp585.csv"
+      }
+    }
+  }
 };
