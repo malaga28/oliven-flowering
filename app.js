92758ec53ea7c75bd6cca20935a139c5444ecb0f
malaga28 <sebastianpiasecki2710@gmail.com>
2026-04-12 20:29:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
   maxZoom: 20
 }).addTo(map);
 
+L.control.scale({
+  position: "topright",
+  metric: true,
+  imperial: false,
+  maxWidth: 120
+}).addTo(map);
+
 const periods = [
   "1951-1980",
   "1981-2014",
